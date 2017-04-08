f2cd5f98350c4c30517e9e20b458949faacc4940
Hamster Tian <haotia@gmail.com>
2017-04-08 20:50:06 +0800

    serial: correct serial port initialization sequence
    Look better, do not be a fool.

--- serial_port.c
+++ serial_port.c
@@ -14,7 +14,7 @@ int g_serial_fd = -1;
 int serial_setup(const char* dev_path) {
 	struct termios serial_termios;
 
-	g_serial_fd = open(dev_path, O_RDWR | O_NOCTTY | O_NDELAY);
+	g_serial_fd = open(dev_path, O_RDWR | O_NOCTTY);
 	if (g_serial_fd == -1) {
 		syslog(LOG_CRIT, "could not open %s\n", dev_path);
 		return FAILURE;
@@ -30,15 +30,25 @@ int serial_setup(const char* dev_path) {
 		return FAILURE;
 	}
 
-	/* 115200 8-N-1 */
+	/* 115200 */
 	cfsetspeed(&serial_termios, B115200);
-	serial_termios.c_lflag &= ~ISIG & ~ICANON & ~ECHO & ~ECHOE;
-	serial_termios.c_iflag &= ~INPCK & ~INLCR & ~ICRNL & ~IXON & ~IXANY & ~IXOFF;
+
+	/* Noncanonical mode, no echo */
+	serial_termios.c_lflag &= ~(ISIG | ICANON | ECHO | ECHOE);
+
+	/* No XON/XOFF flow control */
+	serial_termios.c_iflag &= ~(INPCK | INLCR | ICRNL | IXON | IXANY | IXOFF);
+
+	/* 8-N-1 */
+	serial_termios.c_cflag &= ~(CSIZE | CSTOPB | PARENB);
 	serial_termios.c_cflag |= CS8 | CREAD | CLOCAL;
-	serial_termios.c_cflag &= ~CSTOPB & ~PARENB;
-	serial_termios.c_oflag &= ~OPOST & ~ONLCR & ~OCRNL;
-	serial_termios.c_cc[5] = VINTR;
-	serial_termios.c_cc[6] = VINTR;
+
+	/* No output processing */
+	serial_termios.c_oflag &= ~(OPOST | ONLCR | OCRNL);
+
+	/* Use select() to determine if there is data */
+	serial_termios.c_cc[VTIME] = 0;
+	serial_termios.c_cc[VMIN] = 0;
 
 	tcflush(g_serial_fd, TCIFLUSH);
 	if (tcsetattr(g_serial_fd, TCSANOW, &serial_termios)) {
@@ -55,5 +65,5 @@ void serial_close() {
 }
 
 int serial_write(const unsigned char* data, int len) {
-    return write(g_serial_fd, data, len);
+	return write(g_serial_fd, data, len);
 }
