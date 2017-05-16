b2baf139e4e5099b51f60f0a23ee4b80b90957f6
小山 <lufanzhong@gmail.com>
2017-05-17 02:23:49 +0800

    Update update_oui.sh

--- etc/oui/update_oui.sh
+++ etc/oui/update_oui.sh
@@ -2,9 +2,7 @@
 wget https://cdn.mivm.cn/OpenWrt/k3screenctrl/oui.txt.md5 -O /tmp/oui.txt.md5
 if [ -s "/tmp/oui.txt.md5" ]; then
 	md5=$(cat /tmp/oui.txt.md5 | awk '{print $2}' | tr '[A-Z' '[a-z]')
-	echo $md5
 	md5sum=$(md5sum /etc/oui/oui.txt | awk '{print $1}')
-	echo $md5sum
 	if [ "$md5" != "$md5sum" ]; then
 		wget https://cdn.mivm.cn/OpenWrt/k3screenctrl/oui.txt -O /tmp/oui.txt
 		if [ -s "/tmp/oui.txt" ]; then
