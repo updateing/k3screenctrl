5b7753bb0fcd216c42a99857eb1f567ca3d36c36
小山 <lufanzhong@gmail.com>
2017-05-18 23:24:56 +0800

    Update host.sh

--- lib/k3screenctrl/host.sh
+++ lib/k3screenctrl/host.sh
@@ -20,20 +20,21 @@ do
 		echo 0 >> /tmp/lan_speed/${arp_ip[i]}
 		echo 0 >> /tmp/lan_speed/${arp_ip[i]}
 	fi
-	#判断设备流量监视规则是否存在，不存在则添加
 done
 
-echo ${#online_list[@]}
-#设备在线总数
+if [ -s "/tmp/lan_online_list.temp" ]; then
+	cat /tmp/lan_online_list.temp
+	rm /tmp/lan_online_list.temp
+	exit 0
+else
+	echo ${#online_list[@]}
+fi
 
 for ((i=0;i<${#online_list[@]};i++))
 do
-	hostname=$(cat /tmp/dhcp.leases | grep ${online_list[i]} | awk '{print $4}')
+	hostname[i]=$(cat /tmp/dhcp.leases | grep ${online_list[i]} | awk '{print $4}')
 	hostmac=${mac_online_list[i]//:/} && hostmac=${hostmac:0:6}
-	logo=$(grep -n -i "$hostmac" /etc/oui/oui.txt) && logo=${logo:0:2} && logo=${logo//:/}
-	#oui 下载地址:https://cdn.mivm.cn/OpenWrt/k3screenctrl/oui.txt
-	#oui md5 校验:https://cdn.mivm.cn/OpenWrt/k3screenctrl/oui.txt.md5
-	#oui 来源:斐讯官方 + 个人整理 + 用户反馈
+	logo[i]=$(grep -i "$hostmac" /etc/oui/oui.txt | awk '{print $1}')
 
 	last_speed_time=$(cut -d$'\n' -f,1 /tmp/lan_speed/${online_list[i]})
 	last_speed_up=$(cut -d$'\n' -f,2 /tmp/lan_speed/${online_list[i]})
@@ -45,38 +46,42 @@ do
 	if [ $time_s -eq 0 ];then
 		time_s=1
 	fi
-	up_sp=$((($now_speed_up - $last_speed_up) / $time_s))
-	dw_sp=$((($now_speed_dw - $last_speed_dw) / $time_s))
+	up_sp[i]=$((($now_speed_up - $last_speed_up) / $time_s))
+	dw_sp[i]=$((($now_speed_dw - $last_speed_dw) / $time_s))
 	echo $now_speed_time > /tmp/lan_speed/${online_list[i]}
 	echo $now_speed_up >> /tmp/lan_speed/${online_list[i]}
 	echo $now_speed_dw >> /tmp/lan_speed/${online_list[i]}
-	#依照前后相差的时间差来计算出平均速度，跟 WAN.sh 原理一样
 
-	if [ -z "$hostname" ]; then
+	if [ -z "${hostname[i]}" -o "${hostname[i]}" = "*" ]; then
 		hostname="Unknown"
 	fi
-	if [ -z "$logo" ]; then
-		logo="0"
+	if [ -z "${logo[i]}" ]; then
+		logo[i]="0"
 	fi
-	echo $hostname
-	echo $dw_sp
-	echo $up_sp
-	echo $logo
+	echo ${hostname[i]}
+	echo ${dw_sp[i]}
+	echo ${up_sp[i]}
+	echo ${logo[i]}
 done
 
-last_arp_refresh_time=$(cat /tmp/arp_refresh_time)
 now_arp_refresh_time=$(date +%s)
-if [ -z "$last_arp_refresh_time" ]; then
-	echo $now_arp_refresh_time > /tmp/arp_refresh_time
-elif [ $(($now_arp_refresh_time - $last_arp_refresh_time)) -ge 600 ]; then
-	for ((i=0;i<${#online_list[@]};i++))
-	do
-		arp -d ${online_list[i]}
-	done
+if [ -s "/tmp/arp_refresh_time" ]; then
+	last_arp_refresh_time=$(cat /tmp/arp_refresh_time)
+	if [ $(($now_arp_refresh_time - $last_arp_refresh_time)) -ge 600 ]; then
+		echo ${#online_list[@]} > /tmp/lan_online_list.temp
+		for ((i=0;i<${#online_list[@]};i++))
+		do
+			arp -d ${online_list[i]}
+			echo ${hostname[i]} >> /tmp/lan_online_list.temp
+			echo ${dw_sp[i]} >> /tmp/lan_online_list.temp
+			echo ${up_sp[i]} >> /tmp/lan_online_list.temp
+			echo ${logo[i]} >> /tmp/lan_online_list.temp
+		done
+		echo 0 >> /tmp/lan_online_list.temp
+		echo $now_arp_refresh_time > /tmp/arp_refresh_time
+	fi
+else
 	echo $now_arp_refresh_time > /tmp/arp_refresh_time
 fi
-#设备异常断开后，ARP路由表不会更新设备状态，所以每10分钟彻底更新一次路由表状态，需要ARP二进制的支持，自定义编译BusyBox即可。
 
 echo 0
-#屏幕控制程序的一个BUG，最后不输出一行就显示错误，不知道是什么鬼
-
