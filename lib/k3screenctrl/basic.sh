33cdea9d19d9913e89c7141afdcd3dd4656827c9
小山 <lufanzhong@gmail.com>
2017-05-25 12:08:02 +0800

    Update basic.sh

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -7,17 +7,12 @@ PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
 WAN_IFNAME=$(uci get network.wan.ifname)
 MAC_ADDR=$(ifconfig $WAN_IFNAME | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
 
-HW_VERSION_CACHE_FILE=/tmp/hw_version
-if [ -e "$HW_VERSION_CACHE_FILE" ]; then
-    HW_VERSION=$(cat $HW_VERSION_CACHE_FILE)
-else
-    HW_VERSION=$(nvram get hd_version)
-    echo $HW_VERSION > $HW_VERSION_CACHE_FILE
-fi
+CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
 
+HW_VERSION=${LEDE_DEVICE_REVISION:0:2}
 FW_VERSION=${BUILD_ID:0:17}
 
 echo $PRODUCT_NAME
-echo $HW_VERSION
+echo $HW_VERSION $CPU_TEMP
 echo $FW_VERSION
 echo $MAC_ADDR
